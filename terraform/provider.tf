79600c2e143fbe2810ebb670ca28b7af42c1b6c5
Salvadortarrio <salvador.tarrio@campusdual.com>
2024-12-13 16:57:39 +0100

    profile modified

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,6 @@
 # Configuración del proveedor AWS
 provider "aws" {
   region = var.aws_region  # Usamos una variable para la región, que podemos definir en variables.tf
-  profile = "default"
+  #profile = "default"
+  #quitar profile si se compila desde la nube
 }
