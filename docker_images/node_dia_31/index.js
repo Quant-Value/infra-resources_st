88fe6bcc955a5101b6bf813cf175f8f5000906ea
Salvadortarrio <salvador.tarrio@campusdual.com>
2025-02-03 17:16:13 +0100

    deployando

--- docker_images/node_dia_31/index.js
+++ docker_images/node_dia_31/index.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path'); // Para manejar las rutas de los archivos
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public'));
-
+// Ruta principal
 app.get('/', (req, res) => {
   res.send('Chat app is running!');
 });
 
+// Ruta para el "tesoro" que sirve el archivo index.html
 app.get('/treasure', (req, res) => {
-  res.sendFile(__dirname + 'index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));  // Correcta concatenación de la ruta
 });
 
-server.listen(3000, () => {
+// Iniciar el servidor en todas las interfaces de red
+server.listen(3000, '0.0.0.0', () => {
   console.log('Chat app listening on port 3000');
 });
 
+// Configuración de WebSockets (Socket.io)
 io.on('connection', (socket) => {
   console.log('A user connected');
   
@@ -31,5 +34,3 @@ io.on('connection', (socket) => {
     console.log('User disconnected');
   });
 });
-
-
